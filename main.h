505535974f220a4d46f30e248c15bd13dd1d8feb
Eliab E <eliaberango@gmail.com>
2023-03-25 09:02:14 -0400

    Add main.h file

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  * struct handler - Struct handler
+  *
+  * @cs: Character specifier
+  * @func: The function associated
+  */
+typedef struct handler
+{
+	char *cs;
+	int (*func)(va_list *);
+} handler_t;
+
+int _putchar(char c);
+int c_hand(va_list *ap);
+int s_hand(va_list *ap);
+int (*get_cs_handler(const char *cs))(va_list *);
+int _printf(const char *format, ...);
+
+
+
+
+#endif
