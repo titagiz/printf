cd32f1a61bd1d1a171a34076e00dea549f36e443
Eliab E <eliaberango@gmail.com>
2023-03-25 09:05:07 -0400

    Add get_cs_handler.c which returns a handler for the corresponding conversion specifier

---
+++ get_cs_handler.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * get_cs_handler - selects the correct function to perform
+ *                  the operation asked by the user
+ * @cs: conversion specifier
+ *
+ * Return: a pointer to the function that corresponds to
+ *         the operator given as a parameter
+ */
+int (*get_cs_handler(const char *cs))(va_list *)
+{
+	handler_t handler[] = {
+		{"c", c_hand},
+		{"s", s_hand},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (handler[i].cs)
+	{
+		if (*(handler[i].cs) == *cs)
+			return (handler[i].func);
+		i++;
+	}
+	return (NULL);
+}
