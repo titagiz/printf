edcc0b20ba41582d4b34353d4c8ead5a850d4824
Yenealem Gizachew <yenealemgizachew123@gmail.com>
2023-03-26 02:21:40 -0700

    tasks

--- main.h
+++ main.h
@@ -17,6 +17,7 @@ int _putchar(char c);
 int handle_char(va_list *ap);
 int handle_str(va_list *ap);
 int handle_mod(va_list *ap);
+int handle_dec(va_list d);
 int (*get_cs_handler(const char *cs))(va_list *);
 int _printf(const char *format, ...);
 
