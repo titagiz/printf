cb140020df328696561d071f9fc5fdb43319b012
Eliab E <eliaberango@gmail.com>
2023-03-27 04:25:29 -0400

    Add prototpe of newly added function

--- main.h
+++ main.h
@@ -27,6 +27,7 @@ int handle_oct(va_list *ap);
 int handle_udec(va_list *ap);
 int handle_hex(va_list *ap);
 int handle_heX(va_list *ap);
+int handle_Str(va_list *ap);
 int (*get_cs_handler(const char *cs))(va_list *);
 int _printf(const char *format, ...);
 
