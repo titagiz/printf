6dfa35409d2298adde3afd03d9f25bc67195854f
Yenealem Dula <122959536+titagiz@users.noreply.github.com>
2023-03-28 11:24:08 -0700

    Update handle_rot13.c

--- handle_rot13.c
+++ handle_rot13.c
@@ -17,7 +17,7 @@ int handle_rot13(va_list *ap, cs_modifier_t cs_mod)
 	(void) cs_mod;
 	r = va_arg(*ap, char *);
 	if (r == NULL)
-		r = "(null)";
+		r = "(AHYY)";
 	for (j = 0; r[j] != '\0'; j++)
 	{
 		for (i = 0; input[i] != '\0'; i++)
