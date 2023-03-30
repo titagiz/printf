6e512151e28b06c2c117ccc76b57c413887ae562
Eliab E <eliaberango@gmail.com>
2023-03-30 19:43:34 -0400

    Fix error in string which large size

--- handle_string.c
+++ handle_string.c
@@ -26,6 +26,31 @@ void char_to_hex(char c)
 		_putchar(tmp + 55);
 }
 /**
+ * handle_S - handles 'S' conversion specifier
+ * @s: string to print
+ *
+ * Return: Number of characters printed
+ */
+int handle_S(char *s)
+{
+	int i = 0, count = 0;
+
+	while (s[i])
+	{
+		if (((s[i] > 0 && s[i] < 32) || s[i] >= 127))
+		{
+			char_to_hex(s[i]);
+			count += 3;
+		}
+		else
+		{
+			_putchar(s[i]);
+		}
+		i++;
+	}
+	return (i + count);
+}
+/**
  * handle_s_S - handles 's' and 'S' conversion specifier
  * @arg: point to arguments structure
  *
@@ -41,28 +66,27 @@ int handle_s_S(arg_t *arg)
 	i = count = 0;
 	if (*(arg->cs) == 'S')
 	{
-		while (s[i])
-		{
-			if (((s[i] > 0 && s[i] < 32) || s[i] >= 127))
-			{
-				char_to_hex(s[i]);
-				count += 3;
-			}
-			else
-			{
-				_putchar(s[i]);
-			}
-			i++;
-		}
+		i = handle_S(s);
 	}
 	else
 	{
-		for (; s[i]; i++)
-			arg->buff[i] = s[i];
-		if (arg->field_wd > i)
-			for (j = 0; j < arg->field_wd - i; j++)
-				count += _putchar(' ');
-		i = write(1, arg->buff, i);
+		if (arg->field_wd)
+		{
+			for (; s[i]; i++)
+				arg->buff[i] = s[i];
+			if (arg->field_wd > i)
+				for (j = 0; j < arg->field_wd - i; j++)
+					count += _putchar(' ');
+			i = write(1, arg->buff, i);
+		}
+		else
+		{
+			while (s[i])
+			{
+				_putchar(s[i]);
+				i++;
+			}
+		}
 	}
 	return (i + count);
 }
